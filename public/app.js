11873d462e21b3004bb161f435dda76e9022ba2d
loganss1999 <lsmith5@stevens.edu>
2022-04-25 23:13:03 -0400

    bin files

---
+++ public/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', (_) => {
+	let websocket = new WebSocket("wss://" + window.location.host + "/websocket");
+	let room = document.getElementById("chat-text");
+	websocket.addEventListener("message", function (e) {
+		let data = JSON.parse(e.data);
+		let chatContent = `<p><strong>${data.username}</strong>: ${data.text}</p>`;
+		room.insertAdjacentHTML( 'afterbegin', chatContent );
+		room.scrollTop = room.ScrollHeight; //auto scroll to bottom
+	});
+	
+	let form = document.getElementById("input-form");
+	form.addEventListener("submit", function (event) {
+		event.preventDefault();
+		let username = document.getElementById("input-username");
+		let text = document.getElementById("input-text");
+		websocket.send(
+			JSON.stringify({
+				username: username.value,
+				text: text.value,
+			})
+		);
+		text.value = "";
+	});
+});
