498d56ab833b20df66ffd53d70f1233ba7cadf0e
loganss1999 <lsmith5@stevens.edu>
2022-04-28 14:25:10 -0400

    gifs

--- public/app.js
+++ public/app.js
@@ -9,12 +9,17 @@ class Spot {
 
 let markers = new Map();
 
+var gif1 = document.getElementById("gif1")
+gif1.style.visibility = 'hidden';
+var gif2 = document.getElementById("gif2")
+gif2.style.visibility = 'hidden';
+
 window.addEventListener('DOMContentLoaded', (_) => {
 	let websocket = new WebSocket("wss://" + window.location.host + "/websocket");
 	let gif = document.getElementById("parking-space");
 	websocket.addEventListener("message", function (e) {
 		let data = JSON.parse(e.data);
-		let parkingSpace = new Spot(${data.X},${data.Y},${data.size},${data.time})
+		let parkingSpace = new Spot(${data.Y},${data.X},${data.size},${data.time})
 	});
 	
 	let form = document.getElementById("input-form");
@@ -22,6 +27,14 @@ window.addEventListener('DOMContentLoaded', (_) => {
     event.preventDefault();
     let car = document.getElementById("car")
     if (car.value != 0) {
+		if(car.value == 1) {
+			gif1.style.visibility = 'visible';
+			gif2.style.visibility = 'hidden';
+		}
+		if(car.value == 2) {
+			gif2.style.visibility = 'visible';
+			gif1.style.visibility = 'hidden';
+		}
 		websocket.send(
 		  car: car.value,
 		);
