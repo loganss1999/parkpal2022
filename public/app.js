67706a36971b513c5a3ba995d1594d75e09f80b4
loganss1999 <lsmith5@stevens.edu>
2022-04-28 13:40:43 -0400

    update index

--- public/app.js
+++ public/app.js
@@ -11,11 +11,21 @@ let markers = new Map();
 
 window.addEventListener('DOMContentLoaded', (_) => {
 	let websocket = new WebSocket("wss://" + window.location.host + "/websocket");
-	let room = document.getElementById("parking-space");
+	let gif = document.getElementById("parking-space");
 	websocket.addEventListener("message", function (e) {
 		let data = JSON.parse(e.data);
 		let parkingSpace = new Spot(${data.X},${data.Y},${data.size},${data.time})
 	});
 	
+	let form = document.getElementById("input-form");
+	form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let car = document.getElementById("car")
+    if (car.value != 0) {
+		websocket.send(
+		  car: car.value,
+		);
+	}
+	car.value = 0;
 	
 });
