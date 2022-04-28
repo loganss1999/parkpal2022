7e6f33bceb50c62aeb7a572fa23434e4c4d8750f
loganss1999 <lsmith5@stevens.edu>
2022-04-28 11:43:54 -0400

    pls work

--- SpaceDetector.py
+++ SpaceDetector.py
@@ -4,13 +4,15 @@ from numpy import ones,vstack,arctan
 from numpy.linalg import lstsq
 from math import sqrt,dist
 
+
+
 def defineSpace(x1, y1, x2, y2, size1, size2):
     size = dist([x1,y1],[x2,y2]) - (size1+size2)/2
     x = (x1+x2)/2
     y = (y1+y2)/2
     return [round(size,3),round(x,3),round(y,3)]
 
-def findSpace(sample, xL, yL, xR, yR, orientation):
+def findSpace(nusc, sample, xL, yL, xR, yR, orientation):
     points = [(xL,yL),(xR,yR)]
     x_coords, y_coords = zip(*points)
     A = vstack([x_coords,ones(len(x_coords))]).T
@@ -29,12 +31,18 @@ def findSpace(sample, xL, yL, xR, yR, orientation):
     # print(cars)
     cars.sort()
     spaces = []
-    spaces.append(defineSpace(xL,yL,cars[0][0],cars[0][1],0,cars[0][2]))
+#     space = defineSpace(xL,yL,cars[0][0],cars[0][1],0,cars[0][2])
+#     if(space[0] > 2.5):
+#         spaces.append(space)
     i = 0
     l = len(cars)
     while i < l-1:
-        spaces.append(defineSpace(cars[i][0],cars[i][1],cars[i+1][0],cars[i+1][1],cars[i][2],cars[i+1][2]))
+        space = defineSpace(cars[i][0],cars[i][1],cars[i+1][0],cars[i+1][1],cars[i][2],cars[i+1][2])
+        if(space[0] > 2.5):
+            spaces.append(space)
         i = i + 1
-    spaces.append(defineSpace(cars[i][0],cars[i][1],xR,yR,cars[0][2],0))
+#     space = defineSpace(cars[i][0],cars[i][1],xR,yR,cars[0][2],0)
+#     if(space[0] > 2.5):
+#             spaces.append(space)
     
     return spaces
