e51ef2672d3496b79641c569a95c8f6d61818039
loganss1999 <lsmith5@stevens.edu>
2022-04-28 17:28:04 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 let markers = new Map();
 
 window.addEventListener('DOMContentLoaded', (_) => {
+
 	let websocket = new WebSocket("wss://" + window.location.host + "/websocket");
 	let gif = document.getElementById("parking-space");
 	var gif1 = document.getElementById("gif1")
@@ -27,9 +28,9 @@ window.addEventListener('DOMContentLoaded', (_) => {
 		scene: activeElement.value
       })
     );
+	});
 	//end of sending
-	
-	 var map = L.map('map').setView([40.7440, -74.0324], 15);
+	  	var map = L.map('map').setView([42.3463, -71.0421], 15);
       L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=DNCiULABgsnLAzJKiWCp',{
         tileSize: 512,
         zoomOffset: -1,
@@ -37,7 +38,6 @@ window.addEventListener('DOMContentLoaded', (_) => {
         attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
         crossOrigin: true
       }).addTo(map);
-	  
 	  var greenIcon = L.icon({
     iconUrl: 'https://i.ibb.co/CQ1sxd0/Map-Marke2r-PN2-G-Clipart-removebg-preview.png',
     shadowUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAApCAQAAAACach9AAACMUlEQVR4Ae3ShY7jQBAE0Aoz/f9/HTMzhg1zrdKUrJbdx+Kd2nD8VNudfsL/Th///dyQN2TH6f3y/BGpC379rV+S+qqetBOxImNQXL8JCAr2V4iMQXHGNJxeCfZXhSRBcQMfvkOWUdtfzlLgAENmZDcmo2TVmt8OSM2eXxBp3DjHSMFutqS7SbmemzBiR+xpKCNUIRkdkkYxhAkyGoBvyQFEJEefwSmmvBfJuJ6aKqKWnAkvGZOaZXTUgFqYULWNSHUckZuR1HIIimUExutRxwzOLROIG4vKmCKQt364mIlhSyzAf1m9lHZHJZrlAOMMztRRiKimp/rpdJDc9Awry5xTZCte7FHtuS8wJgeYGrex28xNTd086Dik7vUMscQOa8y4DoGtCCSkAKlNwpgNtphjrC6MIHUkR6YWxxs6Sc5xqn222mmCRFzIt8lEdKx+ikCtg91qS2WpwVfBelJCiQJwvzixfI9cxZQWgiSJelKnwBElKYtDOb2MFbhmUigbReQBV0Cg4+qMXSxXSyGUn4UbF8l+7qdSGnTC0XLCmahIgUHLhLOhpVCtw4CzYXvLQWQbJNmxoCsOKAxSgBJno75avolkRw8iIAFcsdc02e9iyCd8tHwmeSSoKTowIgvscSGZUOA7PuCN5b2BX9mQM7S0wYhMNU74zgsPBj3HU7wguAfnxxjFQGBE6pwN+GjME9zHY7zGp8wVxMShYX9NXvEWD3HbwJf4giO4CFIQxXScH1/TM+04kkBiAAAAAElFTkSuQmCC',
@@ -67,7 +67,6 @@ function onMouseover(e) {
 
       var marker = L.marker([40.739098, -74.032499], {icon: greenIcon}).addTo(map).on('click', onClick);
 	
-	});
 		//receive coordinates from websocket
 	websocket.addEventListener("message", function (e) {
 		let data = JSON.parse(e.data);
